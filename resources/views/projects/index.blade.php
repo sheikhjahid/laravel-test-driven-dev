0cc4841fb35ce2001d1b36b22aa7f82a5a0cf8f4
SheikhJahid <jahid@itobuz.com>
2019-09-03 17:19:49 +0530

    Decoration and css

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,21 +1,46 @@
 @extends('layouts.app')
 
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 2rem !important;
+  text-align: center;
+  background-color: #f1f1f1;
+  border-radius: 1.25rem;
+}
+.column {
+  float: left;
+  width: 50%;
+  padding: 10px;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+</style>
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="page-header" style="display: flex;">
 
-                    <h2 style="margin:right;">All Projects</h2>
+                    <h2 style="color:grey;font-size:20px;margin:left;">My Projects</h2>
                     
-                    <a style="margin:auto;" href="{{ url('/projects/create') }}">Create Project</a>
+                    <a style="text-decoration:none;color:grey;margin:auto;" href="{{ url('/projects/create') }}">Create Project</a>
                 </div>
 
-                <div style="display: flex;">
+                <div class="row">
                 @foreach($projects as $project)
-                <div class="bg-white mr-3 shadow" style="border-radius:41px;width:33.33%;padding:2rem !important;">
-                    <h3 style="margin-bottom:20px;font-size:normal;"><a href="{{url('project',[$project->id])}}">{{ $project->title }}</a></h3>
-                    <div style="color:grey;">{{ str_limit($project->description, 40) }}</div>
+                <div class="column">
+
+                    <div class="card box bg-white shadow">
+                        <h3 style="margin-bottom:20px;font-size:normal;"><a href="{{url('project',[$project->id])}}">{{ $project->title }}</a></h3>
+                        <div style="color:grey;">{{ str_limit($project->description, 40) }}</div>
+                    </div>
                 </div>
                 @endforeach
                  </div>

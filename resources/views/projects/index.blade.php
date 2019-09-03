c7cd56c240ae5d0035f4f1e6a48b32faefb9126e
SheikhJahid <jahid@itobuz.com>
2019-09-03 17:44:04 +0530

    Grid spaciing and Card Tweeks

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,36 +1,14 @@
 @extends('layouts.app')
 
-<style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  padding: 2rem !important;
-  text-align: center;
-  background-color: #f1f1f1;
-  border-radius: 1.25rem;
-}
-.column {
-  float: left;
-  width: 50%;
-  padding: 10px;
-}
-
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-</style>
-
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="page-header" style="display: flex;">
 
-                    <h2 style="color:grey;font-size:20px;margin:left;">My Projects</h2>
+                    <h2>My Projects</h2>
                     
-                    <a style="text-decoration:none;color:grey;margin:auto;" href="{{ url('/projects/create') }}">Create Project</a>
+                    <a class="btn btn-primary create-projects" href="{{ url('/projects/create') }}">Create Project</a>
                 </div>
 
                 <div class="row">
@@ -38,7 +16,8 @@
                 <div class="column">
 
                     <div class="card box bg-white shadow">
-                        <h3 style="margin-bottom:20px;font-size:normal;"><a href="{{url('project',[$project->id])}}">{{ $project->title }}</a></h3>
+                        <a href="{{url($project->path())}}" style="text-decoration: none">
+                        <h3 style="margin-bottom:20px;font-size:normal;border-left: 3px skyblue solid;">{{ $project->title }}</h3></a>
                         <div style="color:grey;">{{ str_limit($project->description, 40) }}</div>
                     </div>
                 </div>
